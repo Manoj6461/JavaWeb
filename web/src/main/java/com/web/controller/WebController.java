1f669ca125a21622a92352ef78435828b9e15de7
Manoj Kumar <manojkumar.j.6461@gmail.com>
2024-11-10 14:14:56 +0530

    sample get mapping

--- web/src/main/java/com/web/controller/WebController.java
+++ web/src/main/java/com/web/controller/WebController.java
@@ -1,5 +1,7 @@
 package com.web.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("web")
 public class WebController {
 
+    @GetMapping(value = "/home")
+    public ResponseEntity<String> getMapping() {
+        return ResponseEntity.ok("Hello Manoj!!! ");
+    }
 
 }
--- web/src/main/java/com/web/model/User.java
+++ web/src/main/java/com/web/model/User.java
@@ -1,4 +1,16 @@
 package com.web.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    private int id;
+    private String name;
+    private  int age;
+    private String dept;
 }
